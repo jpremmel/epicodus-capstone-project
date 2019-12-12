3f2fa2a407f218bfb2253f6597da6837652a5b66
Jacqueline Remmel <jacqueline@>
2019-12-11 16:42:13 -0800

    Start Redux structure

--- frontend-react/src/components/ProfilePage.jsx
+++ frontend-react/src/components/ProfilePage.jsx
@@ -18,7 +18,7 @@ function ProfilePage() {
       <p><strong>Last name:</strong> Remmel</p>
       <p><strong>Email address:</strong> sylvie@remmel.com</p>
       <p><strong>LinkedIn URL:</strong> www.linkedin/in/sylvie-remmel</p>
-      <p><strong>Bio:</strong> THis is where the brief bio will go. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <p><strong>Bio:</strong> This is where the brief bio will go. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
       <Link to='/edit'><button style={btnStyle} className='waves-effect waves-light btn-large'>Edit Profile</button></Link>
     </div>
   );
